4b103d1561e7f6103ef9de88cdaa8e57a86bf6c0
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-04-26 14:09:52 -0700

    reinit project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # soop
 simple object oriented programming
+
+
+# credits
+project gen - https://createapp.dev/
+bundler - https://parceljs.org/
+lang - https://www.typescriptlang.org/
--- src/v4/IClass.ts
+++
@@ -1,8 +0,0 @@
-import { ITranspilerTarget } from "./Transpiler";
-
-export default interface IClass extends ITranspilerTarget
-{
-    name: string,
-    extends: IClass;
-    blocks: { desc: string, owner: string } []; // block desc & class where it was declared
-}
